7c3c5f0c22e090826e74e80af669560b481f3910
gonatae2-hash <gonatae2@gmail.com>
2026-04-28 17:23:20 +0900

    [Day02-practice] feat: PostsRepository.findByAuthor 쿼리 메서드 추가

--- src/main/java/com/jojoldu/book/springboot/domain/posts/PostsRepository.java
+++ src/main/java/com/jojoldu/book/springboot/domain/posts/PostsRepository.java
@@ -1,12 +1,14 @@
 package com.jojoldu.book.springboot.domain.posts;
 
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;  // ← 추가
-import java.util.List;                                  // ← 추가
 
 public interface PostsRepository extends JpaRepository<Posts, Long> {
 
     @Query("SELECT p FROM Posts p ORDER BY p.id DESC")  // ← 추가
     List<Posts> findAllDesc();
+
+    List<Posts> findByAuthor(String author);
 
 }
